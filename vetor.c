e97f52ef0bc22c42522cf6cf5f70b3c29214e732
Vitor Quirino <vitorqm05@gmail.com>
2026-05-28 11:23:44 -0300

    Adicionado vetor.c

---
+++ vetor.c
@@ -0,0 +1,43 @@
+#include "vetor.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Vector *vector_create(unsigned int tamanho)
+{
+    Vector *v = malloc(sizeof(Vector));
+
+    if(v == NULL)return NULL;
+
+    v->size = tamanho;
+    v->data = calloc(tamanho, sizeof(int));
+
+    if(v->data == NULL){
+        free(v);
+        return NULL;
+    }
+
+
+    return v;
+
+}
+
+int vector_get(Vector *v, unsigned i)
+{
+     int valor = v->data[i];
+
+     return valor;
+}
+
+void vector_destroy(Vector *v)
+{
+    if(v == NULL)
+    {
+        printf("Falha ao destruir o vetor, vetor inexistente.");
+        return;
+    }
+
+    free(v->data);
+    free(v);
+    
+}
+
