3208a53742eda252c83a51d80185a521231c9a95
Guilherme Camelo Parca <guicparca@gmail.com>
2026-06-14 16:18:30 -0300

    Create batch script for automating experiments
    This batch script automates the compilation of a C project, prepares a log file for results, and executes a series of tests on various input files, logging the output.

---
+++ executar-experimentos.bat
@@ -0,0 +1,44 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Define o nome do arquivo de saida
+set ARQUIVO_LOG=resultados_experimentos.txt
+
+echo ==================================================
+echo  Iniciando Automacao dos Experimentos
+echo ==================================================
+
+:: 1. Compilacao
+echo [1/3] Compilando o projeto...
+gcc -o main.exe *.c -lm
+
+:: Verifica se a compilacao falhou
+if %ERRORLEVEL% NEQ 0 (
+    echo Erro na compilacao! Abortando os testes.
+    exit /b 1
+)
+echo Compilacao concluida com sucesso.
+
+:: 2. Preparacao do arquivo de log
+echo [2/3] Preparando o log de resultados...
+echo RESULTADOS DOS EXPERIMENTOS - ESTRUTURAS DE DADOS II > %ARQUIVO_LOG%
+echo ==================================================== >> %ARQUIVO_LOG%
+
+:: 3. Execucao da Bateria de Testes
+echo [3/3] Executando testes e gravando no log...
+
+:: Array simulado com os arquivos de teste
+set TESTES=inputs\aleatorio_1k.txt inputs\aleatorio_100k.txt inputs\inverso_100k.txt inputs\quase-ordenado_100k.txt inputs\repetidos_100k.txt inputs\adversarial_bubble.txt inputs\adversarial_radix.txt
+
+:: Roda o programa no modo adaptativo para cada arquivo
+for %%T in (%TESTES%) do (
+    echo  -^> Testando: %%T
+    echo. >> %ARQUIVO_LOG%
+    echo ^>^>^> EXPERIMENTO: %%T ^<^<^< >> %ARQUIVO_LOG%
+    main.exe adaptativo %%T >> %ARQUIVO_LOG%
+)
+
+echo ==================================================
+echo  Testes finalizados! Resultados salvos em: %ARQUIVO_LOG%
+echo ==================================================
+pause
